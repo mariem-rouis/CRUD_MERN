7bbccc0c5dcf3d624dbc35cacba5e1a58de5d1eb
mariem-rouis <mariemrouis2017@gmail.com>
2024-06-29 00:27:57 +0100

    get input from  data  using hooks

--- src/Components/AddStud.js
+++ src/Components/AddStud.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 function AddStud() {
+
+    const [inputval, setInputval] = useState({
+        name: "",
+        address: "",
+        subject: "",
+        mobile: ""
+    });
+    const setData = (e) => {
+        console.log("ppppppppppp", e.target.value)
+        const { name, value } = e.target;
+        setInputval((preval) => {
+            return {
+                ...preval, [name]: value
+            }
+        })
+    }
     return (
         <div className='container mt-5'>
             <form className='mx-auto w-50 shadow p-5'>
@@ -9,19 +25,19 @@ function AddStud() {
                 <h1 className='mt-5'>Fill-up Details</h1>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Student Name</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" value={inputval.name} onChange={setData} name="name" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Student Address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" name="address" onChange={setData} value={inputval.address} id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Student Subject</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" name="subject" onChange={setData} value={inputval.subject} id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Student Mobile</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" name="mobile" onChange={setData} value={inputval.mobile} id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
 
 
--- src/Components/Editstud.js
+++ src/Components/Editstud.js
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 export default function EditStud() {
+
+    const [inputval, setInputval] = useState({
+        name: "",
+        address: "",
+        subject: "",
+        mobile: ""
+    });
+    const setData = (e) => {
+        console.log("ppppppppppp", e.target.value)
+        const { name, value } = e.target;
+        setInputval((preval) => {
+            return {
+                ...preval, [name]: value
+            }
+        })
+    }
     return (
         <div className='container mt-5'>
             <form className='mx-auto w-50 shadow p-5'>
                 <Link className=" btn btn-primary" to="/">Home</Link>
-                <h1 className='mt-5'>Edit  Details</h1>
+                <h1 className='mt-5'>Update Details</h1>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Student Name</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" value={inputval.name} onChange={setData} name="name" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Student Address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" name="address" onChange={setData} value={inputval.address} id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Student Subject</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" name="subject" onChange={setData} value={inputval.subject} id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Student Mobile</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" name="mobile" onChange={setData} value={inputval.mobile} id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
 
 
