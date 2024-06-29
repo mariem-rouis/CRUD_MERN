10cfcf1c162c382d83fd60e36de352c341cf6c7f
mariem-rouis <mariemrouis2017@gmail.com>
2024-06-29 01:18:36 +0100

    fix backend dotenv&database

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const dotenv = require("dotenv")
+const app = express();
+dotenv.config();
+
+
+app.use(cors());
+
+mongoose.connect(process.env.Database)
+    .then(() => {
+        console.log(' dataBase connection done');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+app.listen(6000)
